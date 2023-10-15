f49be8b2b579802a096f970c26202073c4cf26b5
LSRapolu <143562870+LSRapolu@users.noreply.github.com>
2023-10-15 17:28:59 -0400

    Update Project3.R

--- uci-bike-sharing-dataset/Project3.R
+++ uci-bike-sharing-dataset/Project3.R
@@ -22,6 +22,37 @@ library(scales)
 library(gridExtra)
 library(grid)
 
+bar_yearly_totals <- bike_df %>%
+  group_by(Year) %>%
+  summarize(Casual_Total = sum(Casual), Registered_Total = sum(Registered))
+
+#Casual VS Registered Users
+# Create a year-wise distribution plot for Casual users with unique year-based colors
+casual_plot <- ggplot(bar_yearly_totals, aes(x = factor(Year), y = Casual_Total, fill = factor(Year))) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_manual(values = c("orange", "purple")) +  # Manually specifying the colors
+  labs(title = "Year-wise Distribution of Casual Users", x = "Year", y = "Casual Count", fill = "Year")
+
+# Customize the y-axis labels to display in standard numeric notation
+casual_plot <- casual_plot + scale_y_continuous(labels = comma)
+
+# Create a year-wise distribution plot for Registered users with unique year-based colors
+registered_plot <- ggplot(bar_yearly_totals, aes(x = factor(Year), y = Registered_Total, fill = factor(Year))) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_manual(values = c("orange", "purple")) +  # Manually specifying the colors
+  labs(title = "Year-wise Distribution of Registered Users", x = "Year", y = "Registered Count", fill = "Year")
+
+# Customize the y-axis labels to display in standard numeric notation
+registered_plot <- registered_plot + scale_y_continuous(labels = comma)
+
+# Display the two plots side by side
+grid.arrange(casual_plot, registered_plot, ncol = 2)
+#==========================================================
+filtered_data <- bike_df %>% filter(Year == 0)
+filtered_data1 <- bike_df %>% filter(Year == 1)
+
+
+
 # Hour Of the Day v/s Count
 hourly_total_counts <- bike_df %>%
   group_by(Hour) %>%
