530a25462060ed760dd0ea76dfd3cbb8da966073
Saisrinivas783 <142942740+Saisrinivas783@users.noreply.github.com>
2023-10-17 16:37:56 -0400

    Update Project3.R

--- uci-bike-sharing-dataset/Project3.R
+++ uci-bike-sharing-dataset/Project3.R
@@ -22,15 +22,14 @@ library(scales)
 library(gridExtra)
 library(grid)
 
+
 bar_yearly_totals <- bike_df %>%
   group_by(Year) %>%
   summarize(Casual_Total = sum(Casual), Registered_Total = sum(Registered))
 
-#Casual VS Registered Users
 # Create a year-wise distribution plot for Casual users with unique year-based colors
 casual_plot <- ggplot(bar_yearly_totals, aes(x = factor(Year), y = Casual_Total, fill = factor(Year))) +
   geom_bar(stat = "identity", position = "dodge") +
-  scale_fill_manual(values = c("orange", "purple")) +  # Manually specifying the colors
   labs(title = "Year-wise Distribution of Casual Users", x = "Year", y = "Casual Count", fill = "Year")
 
 # Customize the y-axis labels to display in standard numeric notation
@@ -39,7 +38,6 @@ casual_plot <- casual_plot + scale_y_continuous(labels = comma)
 # Create a year-wise distribution plot for Registered users with unique year-based colors
 registered_plot <- ggplot(bar_yearly_totals, aes(x = factor(Year), y = Registered_Total, fill = factor(Year))) +
   geom_bar(stat = "identity", position = "dodge") +
-  scale_fill_manual(values = c("orange", "purple")) +  # Manually specifying the colors
   labs(title = "Year-wise Distribution of Registered Users", x = "Year", y = "Registered Count", fill = "Year")
 
 # Customize the y-axis labels to display in standard numeric notation
@@ -52,11 +50,73 @@ filtered_data <- bike_df %>% filter(Year == 0)
 filtered_data1 <- bike_df %>% filter(Year == 1)
 
 
+# Calculate the total Casual and Registered counts for the filtered data
+yearly_totals <- filtered_data %>%
+  group_by(Year) %>%
+  summarize(Casual_Total = sum(Casual), Registered_Total = sum(Registered))
+yearly_totals1 <- filtered_data1 %>%
+  group_by(Year) %>%
+  summarize(Casual_Total1 = sum(Casual), Registered_Total1 = sum(Registered))
 
+# Calculate the percentages for Casual and Registered users
+yearly_totals$Casual_Percentage <- (yearly_totals$Casual_Total / (yearly_totals$Casual_Total + yearly_totals$Registered_Total)) * 100
+yearly_totals$Registered_Percentage <- (yearly_totals$Registered_Total / (yearly_totals$Casual_Total + yearly_totals$Registered_Total)) * 100
+
+yearly_totals1$Casual_Percentage1 <- (yearly_totals1$Casual_Total1 / (yearly_totals1$Casual_Total1 + yearly_totals1$Registered_Total1)) * 100
+yearly_totals1$Registered_Percentage1 <- (yearly_totals1$Registered_Total1 / (yearly_totals1$Casual_Total1 + yearly_totals1$Registered_Total1)) * 100
+# Colors for the pie chart slices
+pie_colors <- c("lightblue", "lightgreen")
+
+# Create a pie chart for Year 0
+labels <- c(
+  paste("Casual: ", scales::percent(yearly_totals$Casual_Percentage / 100), sep = ""),
+  paste("Registered: ", scales::percent(yearly_totals$Registered_Percentage / 100), sep = "")
+)
+pie(c(yearly_totals$Casual_Percentage, yearly_totals$Registered_Percentage), labels = labels, col = pie_colors, main = "Year 0: Casual and Registered Users")
+
+labels <- c(
+  paste("Casual: ", scales::percent(yearly_totals1$Casual_Percentage1 / 100), sep = ""),
+  paste("Registered: ", scales::percent(yearly_totals1$Registered_Percentage1 / 100), sep = "")
+)
+pie(c(yearly_totals1$Casual_Percentage1, yearly_totals1$Registered_Percentage1), labels = labels, col = pie_colors, main = "Year 1: Casual and Registered Users")
+
+
+
+#==========================================================
 # Hour Of the Day v/s Count
+
 hourly_total_counts <- bike_df %>%
   group_by(Hour) %>%
   summarise(total_rental_count = sum(Total_count) / 1000)
+
+hourly_casual_counts <- bike_df %>%
+  group_by(Hour) %>%
+  summarise(casual_count = sum(Casual) / 1000)
+hourly_registered_counts <- bike_df %>%
+  group_by(Hour) %>%
+  summarise(registered_count = sum(Registered) / 1000)
+grid.arrange(
+  ggplot(hourly_casual_counts, aes(x = factor(Hour), y = casual_count)) +
+    geom_bar(stat = "identity", fill = "lightgreen") +
+    labs(
+      title = "Casual Bike Rentals by Hourly",
+      x = "Hour of the Day",
+      y = "Casual Count (Thousands)"
+    ) +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 60, hjust = 1)),
+  
+  ggplot(hourly_registered_counts, aes(x = factor(Hour), y = registered_count)) +
+    geom_bar(stat = "identity", fill = "orange") +
+    labs(
+      title = "Registered Bike Rentals by Hourly",
+      x = "Hour of the Day",
+      y = "Registered Count (Thousands)"
+    ) +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 60, hjust = 1)),
+  ncol=2
+)
 
 ggplot(hourly_total_counts, aes(x = factor(Hour), y = total_rental_count)) +
   geom_bar(stat = "identity", fill = "lightblue") +
@@ -67,7 +127,3 @@ ggplot(hourly_total_counts, aes(x = factor(Hour), y = total_rental_count)) +
   ) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-
-
